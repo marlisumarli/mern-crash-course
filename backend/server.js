d4d5a7325d00ddda453f0d40c77d9087731a62de
marlisumarli <gg.marlisumarli@gmail.com>
2025-04-22 15:40:07 +0700

    Update

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use('/api/products', ProductRoute);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*", (req, res) => {
+    app.get(/^\/(?!api).*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
     });
 }
