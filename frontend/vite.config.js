208a182f23eb4c6a10fc284cf5684338b389dd79
marlisumarli <gg.marlisumarli@gmail.com>
2025-04-22 14:48:57 +0700

    Update error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: process.env.API_URL,
+                target: "http://localhost:8080",
             }
         }
     }
